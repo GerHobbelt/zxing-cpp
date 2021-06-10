1158b444d6d0e6faa2bdf281c41c233fdb2c65c0
nnabeyang <nabeyang@gmail.com>
2021-06-10 18:17:01 +0900

    Add  iOS wrapper

---
+++ wrappers/ios/ZXingReader.cpp
@@ -0,0 +1,24 @@
+#include "include/ZXingReader.h"
+#include "ReadBarcode.h"
+#include "TextUtfEncoding.h"
+
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+#include "BarcodeFormat.h"
+using namespace ZXing;
+
+void read_barcode(const uint8_t* bytes, int32_t width, int32_t height, int32_t channels, Format format, ZXingResult* out) {
+	DecodeHints hints;
+	hints.setFormats(BarcodeFormat(format));
+	hints.setTryHarder(0);
+	const auto imgfmt = channels == 1 ? ImageFormat::Lum : ImageFormat::BGRX;
+	auto r = ReadBarcode({bytes, width, height, imgfmt, width * channels, channels}, hints);
+	strcpy(out->text, TextUtfEncoding::ToUtf8(r.text()).c_str());	
+	out->format = Format(r.format());
+}

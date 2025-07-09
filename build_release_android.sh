f796ba745ed8de05012ab5ce35a179340ad52a20
Lev Evtodienko <levevt2012@gmail.com>
2025-07-09 21:15:58 +0300

    add CmakeLists and 16kb support

--- build_release_android.sh
+++ build_release_android.sh
@@ -18,7 +18,6 @@ sudo rm -r  x86/
 sudo rm -r  x86_64/
 sudo rm -r  release/
 
-
 mkdir v8/
 mkdir v7/
 mkdir x86/
@@ -27,7 +26,8 @@ mkdir release/
 
 cd release/
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog" -DBUILD_FOR_AARM=ON
+# ARM64-v8a build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=arm64-v8a -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog -Wl,-z,max-page-size=16384" -DBUILD_FOR_AARM=ON
 make -j 16
 sudo make install
 
@@ -37,10 +37,10 @@ file ../v8/libZXing.so
 sudo rm -r *
 sudo rm -rf /usr/local/lib/libZXing.so
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog" -DBUILD_FOR_AARM=ON
+# ARMv7a build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog -Wl,-z,max-page-size=16384" -DBUILD_FOR_AARM=ON
 make -j 16
 sudo make install
-
 
 sudo cp /usr/local/lib/libZXing.so ../v7/
 file ../v7/libZXing.so
@@ -48,7 +48,8 @@ file ../v7/libZXing.so
 sudo rm -r *
 sudo rm -rf /usr/local/lib/libZXing.so
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=x86 -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog" -DBUILD_FOR_AARM=ON
+# x86 build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=x86 -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog -Wl,-z,max-page-size=16384" -DBUILD_FOR_AARM=ON
 make -j 16
 sudo make install
 
@@ -58,7 +59,8 @@ file ../x86/libZXing.so
 sudo rm -r *
 sudo rm -rf /usr/local/lib/libZXing.so
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=x86_64 -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog" -DBUILD_FOR_AARM=ON
+# x86_64 build
+cmake .. -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" -DANDROID_ABI=x86_64 -DANDROID_NATIVE_API_LEVEL=21 -DCMAKE_SHARED_LINKER_FLAGS="-llog -Wl,-z,max-page-size=16384" -DBUILD_FOR_AARM=ON
 make -j 16
 sudo make install
 
